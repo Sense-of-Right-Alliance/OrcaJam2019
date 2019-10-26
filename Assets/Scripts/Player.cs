54eb67f5ad0c994d209fba2167615237d20e9f38
NarPar <n.pettyjohn@gmail.com>
2019-10-26 16:33:17 -0700

    Player, Resource UI & Event
    Added player class
    
    Player class has new OnResourceChanged event
    
    Added resource ui pieces  to canvas
    
    UIManager listens for OnResourceChanged event and updates the corresponding player's UI.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    public class ResourceChangedEvent : UnityEvent<int, int> { }
+    public static ResourceChangedEvent OnResourceChanged { get; } = new ResourceChangedEvent();
+
+    [SerializeField] GameObject SelectorPrefab;
+
+    [SerializeField] int resources = 0;
+    [SerializeField] int ID = 0;
+
+    public int Resources
+    {
+        get { return resources; }
+        set
+        {
+            resources = value;
+            OnResourceChanged.Invoke(0, resources);
+        }
+    }
+
+    GameObject selector;
+
+    private void Awake()
+    {
+        selector = (GameObject)Instantiate(SelectorPrefab);
+        selector.GetComponent<PlayerSelector>().Init(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,15 +10,29 @@ public class UIManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI SeasonText;
     [SerializeField] TextMeshProUGUI WeatherText;
 
+    [SerializeField] TextMeshProUGUI ResourceText1;
+    [SerializeField] TextMeshProUGUI ResourceText2;
+    [SerializeField] TextMeshProUGUI ResourceText3;
+    [SerializeField] TextMeshProUGUI ResourceText4;
+
+    TextMeshProUGUI[] ResourceTexts;
+
     // Start is called before the first frame update
     void Start()
     {
         if (YearText == null) YearText = GameObject.Find("Year Text").GetComponent<TextMeshProUGUI>();
         if (SeasonText == null) SeasonText = GameObject.Find("Season Text").GetComponent<TextMeshProUGUI>();
-        if (SeasonText == null) WeatherText = GameObject.Find("Weather Text").GetComponent<TextMeshProUGUI>();
+        if (WeatherText == null) WeatherText = GameObject.Find("Weather Text").GetComponent<TextMeshProUGUI>();
+
+        if (ResourceText1 == null) ResourceText1 = GameObject.Find("Resource1").GetComponentInChildren<TextMeshProUGUI>();
+        if (ResourceText2 == null) ResourceText2 = GameObject.Find("Resource2").GetComponentInChildren<TextMeshProUGUI>();
+        if (ResourceText3 == null) ResourceText3 = GameObject.Find("Resource3").GetComponentInChildren<TextMeshProUGUI>();
+        if (ResourceText4 == null) ResourceText4 = GameObject.Find("Resource4").GetComponentInChildren<TextMeshProUGUI>();
+        ResourceTexts = new TextMeshProUGUI[] { ResourceText1, ResourceText2, ResourceText3, ResourceText4 };
 
         Utility.SeasonManager.OnSeasonChanged.AddListener(UpdateSeason);
         Utility.GameManager.OnYearChanged.AddListener(UpdateYear);
+        Player.OnResourceChanged.AddListener(UpdateResource);
     }
 
     // Update is called once per frame
@@ -40,5 +54,10 @@ public class UIManager : MonoBehaviour
     private void UpdateWeather(SeasonEventType weather)
     {
         WeatherText.text = weather.ToString();
+    }
+
+    private void UpdateResource(int playerId, int amount)
+    {
+        ResourceTexts[playerId].text = amount.ToString();
     }
 }
