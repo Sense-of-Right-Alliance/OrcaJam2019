f9821c04509da1c6de72beba4265933c98bde8a8
NarPar <n.pettyjohn@gmail.com>
2019-10-26 13:54:58 -0700

    Selector and Repairing
    Can now move selector between parts and scarecrows.
    
    Adjusted daynight bg to look a bit better.

--- Assets/PlayerSelector.cs
+++ Assets/PlayerSelector.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class PlayerSelector : MonoBehaviour
 {
     [SerializeField] int InputIndex = 1;
-    [SerializeField] Scarecrow ScarecrowSelected;
+    [SerializeField] int scarecrowIndex = 0;
 
     [SerializeField] int partIndex = 0;
     Transform partSelected;
+    Scarecrow ScarecrowSelected;
 
     bool inputReset = true; // have to set dir/pad back to 0 before moving again. This is weird and I'm sorry.
+
+    GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = Utility.GetGameManager();
+
+        ScarecrowSelected = gameManager.scarecrows[scarecrowIndex];
     }
 
     // Update is called once per frame
@@ -28,20 +33,25 @@ public class PlayerSelector : MonoBehaviour
 
         if (Mathf.Abs(horizontal) > 0)
         {
-            Debug.Log("Player " + InputIndex.ToString() + " Moved horizontal! " + horizontal.ToString());
+            //Debug.Log("Player " + InputIndex.ToString() + " Moved horizontal! " + horizontal.ToString());
             if (inputReset) ChangeScarecrow(horizontal);
+            inputReset = false;
             madeInput = true;
         }
         if (Mathf.Abs(vertical) > 0)
         {
-            Debug.Log("Player " + InputIndex.ToString() + " Moved vertically! " + vertical.ToString());
+            //Debug.Log("Player " + InputIndex.ToString() + " Moved vertically! " + vertical.ToString());
             if (inputReset) ChangeBodyPart(vertical);
+            inputReset = false;
             madeInput = true;
         }
 
         if (Input.GetButtonDown("A"+ InputIndex.ToString()))
         {
-            Debug.Log("Player " + InputIndex.ToString() + " 'A' button pressed!");
+            //Debug.Log("Player " + InputIndex.ToString() + " 'A' button pressed!");
+            ScarecrowPart[] parts = ScarecrowSelected.GetScarecrowPartTransforms();
+            ScarecrowSelected.RepairPart(parts[partIndex].type,1);
+            inputReset = false;
             madeInput = true;
         }
 
@@ -49,27 +59,29 @@ public class PlayerSelector : MonoBehaviour
         {
             inputReset = true;
         } else {
-            inputReset = true;
+            inputReset = false;
         }
     }
 
     private void ChangeScarecrow(float horizontal)
     {
-        if (horizontal > 0)
-        {
-
-        }
+        Scarecrow[] scarecrows = gameManager.scarecrows;
+        scarecrowIndex = horizontal > 0 ? (scarecrowIndex + 1) % scarecrows.Length : scarecrowIndex == 0 ? scarecrows.Length : scarecrowIndex - 1;
+        ScarecrowSelected = scarecrows[scarecrowIndex];
+        UpdateSelection();
     }
 
     private void ChangeBodyPart(float vertical)
     {
-        Transform[] parts = ScarecrowSelected.GetScarecrowPartTransforms();
+        ScarecrowPart[] parts = ScarecrowSelected.GetScarecrowPartTransforms();
+        partIndex = vertical < 0 ? (partIndex + 1) % parts.Length : partIndex == 0 ? parts.Length : partIndex - 1;
+        UpdateSelection();
+    }
 
-        partIndex = vertical > 0 ? (partIndex + 1) % parts.Length : partIndex == 0 ? parts.Length : partIndex - 1;
-
-
-        //Debug.Log(" part index = " + partIndex);
-        partSelected = parts[partIndex];
+    private void UpdateSelection()
+    {
+        ScarecrowPart[] parts = ScarecrowSelected.GetScarecrowPartTransforms();
+        partSelected = parts[partIndex].transform;
 
         Vector3 newPos = new Vector3(partSelected.position.x, partSelected.position.y, transform.position.z);
         transform.position = newPos;
